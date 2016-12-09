d9d36569507fd02829b86cdd30e8b35b609785ff
WataruTaguci <WataruTaguchi.Hornet@gmail.com>
2016-12-10 01:18:29 +0900

    Update 02EasyCipher.py

--- 02EasyChipher.py
+++ 02EasyChipher.py
@@ -1,25 +1,21 @@
-def decipher(_chr, _slideNum, _mode):
-    TargetChr = _chr[0]
-#    print(type(TargetChr))
-    if((TargetChr >= 'a' and TargetChr <= 'z') or (TargetChr >= 'A' and TargetChr <= 'Z')):
-        if(_mode % 2 == 0):
-            if((chr(ord(TargetChr) - _slideNum) < 'a') or (chr(ord(TargetChr) - _slideNum) < 'A')):
-                print(chr(ord('z')-ord(TargetChr)-_slideNum), end='')
-            else:
-                print(chr(ord(TargetChr) - _slideNum), end='')
-        else:
-            if((chr(ord(TargetChr) + _slideNum) > 'z') or (chr(ord(TargetChr) + _slideNum) > 'Z')):
-                print(chr(ord('a')+ord(TargetChr)-_slideNum), end='')
-            else:
-                print(chr(ord(TargetChr) - _slideNum), end='')
+# coding: utf-8
+
+cipher = list("EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT.")
+
+for i in range(len(cipher)):
+    if cipher[i] == ' ':
+        print(" ", end='')
+        pass
 
     else:
-        print(ord(TargetChr), end='')
+        if cipher[i] >= 'a' and cipher[i] <= 'z':
+            if cipher[i] >= chr(ord('n')):
+                print(chr(ord(cipher[i])-13), end='')
+            else:
+                print(chr(ord(cipher[i])+13), end='')
 
-if __name__ == "__main__":
-    cipher = "EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT."
-    
-    cnt = 0
-    for i in cipher:
-        print(decipher(i, ord('v')-ord('i'), cnt))
-        cnt += 1
+        elif cipher[i] >= 'A' and cipher[i] <= 'Z':
+            if cipher[i] >= chr(ord('N')):
+                print(chr(ord(cipher[i])-13), end='')
+            else:
+                print(chr(ord(cipher[i])+13), end='')
